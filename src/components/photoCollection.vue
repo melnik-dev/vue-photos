a844a119035dedd251f2c5bd25f074859df50d1a
Aleksandr <for.melnik.dev@gmail.com>
2022-10-01 13:28:49 +0300

    create project

---
+++ src/components/photoCollection.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="collection">
+    <img class="collection__big" :src="images[0]" alt="Item" />
+    <div class="collection__bottom">
+      <img class="collection__mini" :src="images[1]" alt="Item" />
+      <img class="collection__mini" :src="images[2]" alt="Item" />
+      <img class="collection__mini" :src="images[3]" alt="Item" />
+    </div>
+    <h4>{{ name }}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "photoCollection",
+  props: ["name", "images"]
+}
+</script>
+
+<style scoped>
+
+</style>
